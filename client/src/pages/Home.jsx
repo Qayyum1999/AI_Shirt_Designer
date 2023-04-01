1e1c8c47990c0f8873348bf098eff6eddbef1b88
Qayyum1999 <m.qayyumrazali@gmail.com>
2023-04-01 12:00:15 +0800

    modified frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
         <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
             <img 
-              src='./threejs.png'
+              src='./logo.png'
               alt="logo"
               className="w-8 h-8 object-contain"
             />
@@ -28,7 +28,7 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                CREATE <br className="xl:block hidden" /> YOUR STYLE.
               </h1>
             </motion.div>
             <motion.div
@@ -36,7 +36,8 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              With our new 3D customization tool, you can design a shirt that is truly one-of-a-kind. 
+              <strong> Design your world, wear your imagination</strong>{" "} and express your unique style.
               </p>
 
               <CustomButton 
